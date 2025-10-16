7bc200e4a4222b50311a74f5b1115c7e4ebfe89f
EarlPeacoat95 <earlpeacoat@gmail.com>
2025-10-16 18:43:16 +0100

    went terribly wrong here

--- OOPassignment.py
+++ OOPassignment.py
@@ -1,10 +1,14 @@
 import math
 
+# Begin by establishing a base class from which the rest shall inherit.
+
 class Shape:
     def area(self):
         pass
     def perimeter(self):
         pass
+
+# Now we go into the specific shapes, with the knowledge that they will need both area and perimeter methods.
 
 class Circle(Shape):
     def __init__(self, radius):
@@ -26,9 +30,25 @@ class Rectangle(Shape):
     
     def perimeter(self):
         return 2 * (self.width + self.height)
+
+# For the triangle, we will need to use Herons Formula.
+  
+class Triangle(Shape):
+    def __init__(self,):
+        a = side_one
+        b = side_two
+        c = side_three
+
+        def area(self):
+            return math.sqrt(s * (s - a) * (s - b) * (s - c))
+
+       math.sqrt(s * (s - a) * (s - b) * (s - c))
         
+
+# Below are examples of using these classes and then printing the output.
 
 c = Circle(10)
 print(f"The area of your specified Circle is: {round(c.area())}, and the perimeter is: {round(c.perimeter())}")
 
-
+r = Rectangle(5)
+print (f"The area of your specified Retangle is: {round(r.area())}, and the perimeter is: {round(r.perimeter())}")
